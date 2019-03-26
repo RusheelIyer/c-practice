3f78e704dbbaf4d7cc7217bcc67b7c53f6a84487
Rusheel Iyer <rusheel2911@gmail.com>
2019-03-26 20:41:19 +0100

    ub 1 complete

--- ub-1/p2/parseint.c
+++ ub-1/p2/parseint.c
@@ -1,4 +1,5 @@
 #include "parseint.h"
+#include <string.h>
 
 /*
  * Returns the value of c or -1 on error
@@ -6,6 +7,13 @@
 int parseDecimalChar(char c)
 {
     (void)c;
+
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    else if (c >= 'A' && c <= 'F') {
+        return (c - 'A') + 10;
+    }
 
     return -1;
 }
@@ -18,5 +26,36 @@ int parseInt(char *string)
 {
     (void)string;
 
-    return -1;
+    int numChars = strlen(string);
+
+    int multiplier = 8;
+    if (!(parseDecimalChar(*string) == 0))
+        multiplier = 10;
+
+    for (int i = 0; i < numChars; i++) {
+        char curChar = * (string + i);
+        if (parseDecimalChar(curChar) == -1)
+            return -1;
+        if (multiplier == 8) {
+            if (parseDecimalChar(curChar) > 7)
+                return -1;
+        }
+    }
+
+    int number = 0;
+    int j = 0;
+
+    for (int i = numChars - 1; i >= 0; i--) {
+        int curNumber = parseDecimalChar(*(string + i));
+        int power = 1;
+        for (int k = 0; k < j; k++) {
+            power *= multiplier;
+        }
+
+        number += (curNumber *= power);
+        j++;
+
+    }
+
+    return number;
 }
