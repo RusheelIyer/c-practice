2152e41f8e35c8981afaed5bd78c8d9ecebc49a5
Rusheel Iyer <rusheel2911@gmail.com>
2019-03-26 19:53:20 +0100

    finished tut 00

---
+++ notes.md
@@ -0,0 +1,6 @@
+# Struct
+A `struct` forms a compound type out of several individual variables.
+
+* It allows to store several pieces of data in one variable
+* OSs generally use them for control data structures.
+* individual components of a `struct` are called members
--- tutorial-00/p2/countchr.c
+++ tutorial-00/p2/countchr.c
@@ -1,9 +1,17 @@
 #include "countchr.h"
+#include <string.h>
 
 int countchr(char *string, char c) {
 	(void) string;
 	(void) c;
-	// Add code here.
 
-	return 0;
+    int numChars = strlen(string);
+    int counter = 0;
+
+    for (int i = 0; i < numChars; i++) {
+        if (*(string + i) == c)
+            counter++;
+    }
+
+	return counter;
 }
