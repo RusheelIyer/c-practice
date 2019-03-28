202e3ba9c1341bc7f6df6c5c8bd34dd0fe6531b2
Rusheel Iyer <rusheel2911@gmail.com>
2019-03-28 19:56:54 +0100

    tut 6 complete

--- tut-06/pipes.c
+++ tut-06/pipes.c
@@ -8,5 +8,22 @@ int main(int argc, char const *argv[])
 {
     int pid, pipefd[2];
 
+    // Create the pipe. Parent has write and read end open
     pipe(pipefd);
+
+    pid = fork();
+    if (pid == 0) {
+        dup2(pipefd[WRITE_END], STDOUT_FILENO);
+
+        close(pipefd[READ_END]);
+        close(pipefd[WRITE_END]);
+
+        execlp("/bin/less", "less", NULL);
+    } else {
+        close(pipefd[READ_END]);
+        close(pipefd[WRITE_END]);        
+
+        wait(NULL);
+        wait(NULL);
+    }
 }
