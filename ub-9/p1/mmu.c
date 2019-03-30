69ebc2eee5ee8f144449682254ba41e90e9378d5
Rusheel Iyer <rusheel2911@gmail.com>
2019-03-30 09:51:38 +0100

    ub 9 complete

--- ub-9/p1/mmu.c
+++ ub-9/p1/mmu.c
@@ -10,6 +10,15 @@ static SegmentTable* _table;
 #define SEGMENT_MASK 0xE000u
 #define OFFSET_MASK 0x1FFFu
 
+typedef struct _TLBEntry {
+    uint32_t virtualAddress;
+    uint32_t physicalAddress;
+    int counter;
+} TLBEntry;
+
+static TLBEntry TLB[TLB_SIZE];
+static int count = 0;
+
 void setSegmentTable(SegmentTable* newTable)
 {
     _table = newTable;
@@ -21,7 +30,7 @@ int translateSegmentTable(uint32_t* address)
     if (address == NULL || _table == NULL)
         return -1;
 
-    uint32_t segment = (SEGMENT_MASK & *address) >> OFFSET_BITS;
+    uint32_t segment = *address >> OFFSET_BITS;
     uint32_t offset = OFFSET_MASK & *address;
 
     if (segment >= SEGMENT_COUNT)
@@ -29,7 +38,7 @@ int translateSegmentTable(uint32_t* address)
 
     Segment curSegment = _table->segments[segment];
 
-    if (offset > curSegment.length)
+    if (offset >= curSegment.length)
         return -1;
 
     *address = curSegment.base + offset;
@@ -39,16 +48,49 @@ int translateSegmentTable(uint32_t* address)
 
 void flushTLB(void)
 {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        TLBEntry entry = { 0, 0, 0 };
+        TLB[i] = entry;
+    }
 }
 
 void addToTLB(uint32_t virtual, uint32_t physical)
 {
-    (void)virtual;
-    (void)physical;
+    int nextfree = TLB_SIZE;
+
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (TLB[i].virtualAddress == 0) {
+            nextfree = i;
+            break;
+        }
+    }
+
+    if (nextfree != TLB_SIZE) {
+        TLB[nextfree].virtualAddress = virtual;
+        TLB[nextfree].physicalAddress = physical;
+        TLB[nextfree].counter = count++;
+        return;
+    }
+
+    nextfree = 0;
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (TLB[i].counter < TLB[nextfree].counter) {
+            nextfree = i;
+        }
+    }
+
+    TLB[nextfree].virtualAddress = virtual;
+    TLB[nextfree].physicalAddress = physical;
+    TLB[nextfree].counter = count++;
 }
 
 int translateTLB(uint32_t* address)
 {
-    (void)address;
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (TLB[i].virtualAddress == *address) {
+            return TLB[i].physicalAddress;
+        }
+    }
+
     return -1;
 }
