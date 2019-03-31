9b9cc3c902472234db728416539d6e1564c774bc
Rusheel Iyer <rusheel2911@gmail.com>
2019-03-31 19:04:44 +0200

    parsecopy implementation

--- ub-12/p2/copy.c
+++ ub-12/p2/copy.c
@@ -1,33 +1,50 @@
 #define _POSIX_C_SOURCE 2
 #include "copy.h"
-#include <unistd.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 // Intermediate copy buffer. Use this buffer for your read() and write() calls.
 char _buffer[BUFFER_SIZE];
 
-int parseCopyArgs(int argc, char * const argv[], CopyArgs* args)
+int parseCopyArgs(int argc, char* const argv[], CopyArgs* args)
 {
     if (args == NULL) {
         return -1;
     }
 
     // Initialize with default values
-    args->skip  = 0;
+    args->skip = 0;
     args->count = -1;
 
-    (void) argc;
-    (void) argv;
+    const char* optstring = "s: c:";
+    int arguments = getopt(argc, argv, optstring);
 
-    // ----------------
-    // Parse the arguments.
-    // ----------------
+    while (arguments != -1) {
+        switch (arguments) {
+        case 's':
+            args->skip = atoi(optarg);
+            break;
+        case 'c':
+            args->count = atoi(optarg);
+            break;
+        default:
+            return -1;
+        }
+        arguments = getopt(argc, argv, optstring);
+    }
 
-    return -1;
+    if (optind != argc - 2) {
+        return -1; // We expect two parameters after the options.
+    }
+
+    args->from = argv[optind];
+    args->to   = argv[optind + 1];
+
+    return 0;
 }
 
 int doCopy(CopyArgs* args)
